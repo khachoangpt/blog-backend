fec121a85c1f47d1c2bf325de05dabb9498fec53
Hoang Tifana <hoang@tifana.com>
2024-03-26 17:10:23 +0700

    feat: config oas generator use for nextjs

--- src/utils/generate-oas.ts
+++ src/utils/generate-oas.ts
@@ -17,11 +17,7 @@ const adminOptions: swaggerJSDoc.Options = {
 			},
 		],
 	},
-	apis: [
-		'./src/controllers/admin/**/*.ts',
-		'./src/models/**/*.ts',
-		'./src/constants/enum.ts',
-	],
+	apis: ['./src/controllers/admin/**/*.ts', './src/models/**/*.ts', './src/constants/enum.ts'],
 }
 
 const customerOptions: swaggerJSDoc.Options = {
@@ -38,31 +34,87 @@ const customerOptions: swaggerJSDoc.Options = {
 			},
 		],
 	},
-	apis: [
-		'./src/controllers/customer/**/*.ts',
-		'./src/models/**/*.ts',
-		'./src/constants/enum.ts',
-	],
+	apis: ['./src/controllers/customer/**/*.ts', './src/models/**/*.ts', './src/constants/enum.ts'],
 }
 
-export const generateAdminOas = async () => {
+/**
+ * Generates the OpenAPI Specification (OAS) for the admin API.
+ *
+ * @return {Promise<void>} - A promise that resolves when the OAS has been generated and exported.
+ */
+export const generateAdminOas = async (): Promise<void> => {
 	logger.info('🟣 Generating Admin OAS')
 	const openapiSpecification = swaggerJSDoc(adminOptions)
 	await validateOAS(openapiSpecification, 'Admin')
-	fs.writeFileSync(
-		'./docs/spec.admin.json',
-		JSON.stringify(openapiSpecification, null, 2),
-	)
+	fs.writeFileSync('./docs/spec.admin.json', JSON.stringify(openapiSpecification, null, 2))
+	convertOasForNextJs(openapiSpecification, 'admin')
 	logger.info('⚫️ Exported Admin OAS')
 }
 
-export const generateCustomerOas = async () => {
+/**
+ * Generates the OpenAPI Specification (OAS) for the customer API.
+ *
+ * @return {Promise<void>} - A promise that resolves when the OAS has been generated and exported.
+ */
+export const generateCustomerOas = async (): Promise<void> => {
 	logger.info('🟣 Generating Customer OAS')
 	const openapiSpecification = swaggerJSDoc(customerOptions)
 	await validateOAS(openapiSpecification, 'Customer')
-	fs.writeFileSync(
-		'./docs/spec.customer.json',
-		JSON.stringify(openapiSpecification, null, 2),
-	)
+	fs.writeFileSync('./docs/spec.customer.json', JSON.stringify(openapiSpecification, null, 2))
+	convertOasForNextJs(openapiSpecification, 'customer')
 	logger.info('⚫️ Exported Customer OAS')
+}
+
+/**
+ * Converts the given OpenAPI specification for Next.js usage and writes the result to a JSON file.
+ *
+ * @param {any} oas - the OpenAPI specification to convert
+ * @param {'customer' | 'admin'} type - the type of the Next.js option
+ * @return {void}
+ */
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const convertOasForNextJs = (oas: any, type: 'customer' | 'admin'): void => {
+	const paths = oas.paths
+	const pathsArray = Object.entries(paths).map(([path, method]) => ({ path, method }))
+
+	for (let i = 0; i < pathsArray.length; i++) {
+		const method = pathsArray[i].method as unknown as { parameters: Array<object> }
+		method.parameters = [
+			{
+				in: 'path',
+				name: 'next',
+				description: 'Next.js option',
+				schema: {
+					type: 'object',
+					properties: {
+						revalidate: {
+							type: 'number',
+						},
+						tags: {
+							type: 'array',
+							items: {
+								type: 'string',
+							},
+						},
+					},
+				},
+				required: false,
+			},
+			{
+				in: 'path',
+				name: 'cache',
+				description: 'Next.js option',
+				schema: {
+					type: 'string',
+					enum: ['default', 'force-cache', 'no-cache', 'no-store', 'only-if-cached', 'reload'],
+				},
+				required: false,
+			},
+		]
+	}
+
+	const pathsObj = Object.fromEntries(pathsArray.map((i) => [i.path, i.method]))
+	oas.paths = Object.assign(oas.paths, pathsObj)
+
+	fs.writeFileSync(`./docs/next-spec.${type}.json`, JSON.stringify(oas, null, 2))
 }
